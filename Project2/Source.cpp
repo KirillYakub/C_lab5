58fe6409e679891ecfa10877bf29fb008f939924
KirillYakub <kirillyakubchinskiy@gmail.com>
2020-04-20 18:07:52 +0300

    Добавлен сложный уровень

--- Project2/Source.cpp
+++ Project2/Source.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+///////////////////////////////////////////////////////////
+
 //необходимые структуры;
 struct Birthday
 {
@@ -168,132 +170,138 @@ void Old_time(Pensioner* arr, int size)
 		int count1;
 		count1 = arr[i].DATE.year;
 
-		for (int j = 0; j <= count1; j++)
+		for (int j = 0; j < count1; j++)
 		{
 			if (j % 4 != 0)
 			{
+				
 				arr[i].time += 365;
-				if (j == count1)
+
+				if (j == count1 - 1)
 				{
 					if (arr[i].DATE.month == 1)
-						arr[i].time += 31 + arr[i].DATE.date;
+						arr[i].time += arr[i].DATE.date;
 					if (arr[i].DATE.month == 2)
-						arr[i].time += 28 + 31 + arr[i].DATE.date;
+						arr[i].time += 31 + arr[i].DATE.date;
 					if (arr[i].DATE.month == 3)
-						arr[i].time += 31 * 2 + 28 + arr[i].DATE.date;
+						arr[i].time += 31 + 28 + arr[i].DATE.date;
 					if (arr[i].DATE.month == 4)
-						arr[i].time += 31 * 2 + 28 + 30 + arr[i].DATE.date;
+						arr[i].time += 31 * 2 + 28 + arr[i].DATE.date;
 					if (arr[i].DATE.month == 5)
-						arr[i].time += 31 * 3 + 30 + 28 + arr[i].DATE.date;
+						arr[i].time += 31 * 2 + 30 + 28 + arr[i].DATE.date;
 					if (arr[i].DATE.month == 6)
-						arr[i].time += 31 * 3 + 30 * 2 + 28 + arr[i].DATE.date;
+						arr[i].time += 31 * 3 + 30 + 28 + arr[i].DATE.date;
 					if (arr[i].DATE.month == 7)
-						arr[i].time += 31 * 4 + 30 * 2 + 28 + arr[i].DATE.date;
+						arr[i].time += 31 * 3 + 30 * 2 + 28 + arr[i].DATE.date;
 					if (arr[i].DATE.month == 8)
-						arr[i].time += 31 * 5 + 30 * 2 + 28 + arr[i].DATE.date;
+						arr[i].time += 31 * 4 + 30 * 2 + 28 + arr[i].DATE.date;
 					if (arr[i].DATE.month == 9)
-						arr[i].time += 31 * 5 + 30 * 3 + 28 + arr[i].DATE.date;
+						arr[i].time += 31 * 4 + 30 * 3 + 28 + arr[i].DATE.date;
 					if (arr[i].DATE.month == 10)
-						arr[i].time += 31 * 6 + 30 * 3 + 28 + arr[i].DATE.date;
+						arr[i].time += 31 * 4 + 30 * 4 + 28 + arr[i].DATE.date;
 					if (arr[i].DATE.month == 11)
-						arr[i].time += 31 * 6 + 30 * 4 + 28 + arr[i].DATE.date;
+						arr[i].time += 31 * 5 + 30 * 4 + 28 + arr[i].DATE.date;
 					if (arr[i].DATE.month == 12)
-						arr[i].time += 31 * 7 + 30 * 4 + 28 + arr[i].DATE.date;
+						arr[i].time += 31 * 5 + 30 * 5 + 28 + arr[i].DATE.date;
 				}
 			}
 
 			if (j % 4 == 0)
 			{
-				arr[i].time += 366;
-				if (j == count1)
+				
+			    arr[i].time += 366;
+
+				if (j == count1 - 1)
 				{
 					if (arr[i].DATE.month == 1)
-						arr[i].time += 31 + arr[i].DATE.date;
+						arr[i].time += arr[i].DATE.date;
 					if (arr[i].DATE.month == 2)
-						arr[i].time += 29 + 31 + arr[i].DATE.date;
+						arr[i].time += 31 + arr[i].DATE.date;
 					if (arr[i].DATE.month == 3)
-						arr[i].time += 31 * 2 + 29 + arr[i].DATE.date;
+						arr[i].time += 31 + 29 + arr[i].DATE.date;
 					if (arr[i].DATE.month == 4)
-						arr[i].time += 31 * 2 + 29 + 30 + arr[i].DATE.date;
+						arr[i].time += 31 * 2 + 29 + arr[i].DATE.date;
 					if (arr[i].DATE.month == 5)
-						arr[i].time += 31 * 3 + 30 + 29 + arr[i].DATE.date;
+						arr[i].time += 31 * 2 + 30 + 29 + arr[i].DATE.date;
 					if (arr[i].DATE.month == 6)
-						arr[i].time += 31 * 3 + 30 * 2 + 29 + arr[i].DATE.date;
+						arr[i].time += 31 * 3 + 30 + 29 + arr[i].DATE.date;
 					if (arr[i].DATE.month == 7)
-						arr[i].time += 31 * 4 + 30 * 2 + 29 + arr[i].DATE.date;
+						arr[i].time += 31 * 3 + 30 * 2 + 29 + arr[i].DATE.date;
 					if (arr[i].DATE.month == 8)
-						arr[i].time += 31 * 5 + 30 * 2 + 29 + arr[i].DATE.date;
+						arr[i].time += 31 * 4 + 30 * 2 + 29 + arr[i].DATE.date;
 					if (arr[i].DATE.month == 9)
-						arr[i].time += 31 * 5 + 30 * 3 + 29 + arr[i].DATE.date;
+						arr[i].time += 31 * 4 + 30 * 3 + 29 + arr[i].DATE.date;
 					if (arr[i].DATE.month == 10)
-						arr[i].time += 31 * 6 + 30 * 3 + 29 + arr[i].DATE.date;
+						arr[i].time += 31 * 4 + 30 * 4 + 29 + arr[i].DATE.date;
 					if (arr[i].DATE.month == 11)
-						arr[i].time += 31 * 6 + 30 * 4 + 29 + arr[i].DATE.date;
+						arr[i].time += 31 * 5 + 30 * 4 + 29 + arr[i].DATE.date;
 					if (arr[i].DATE.month == 12)
-						arr[i].time += 31 * 7 + 30 * 4 + 29 + arr[i].DATE.date;
+						arr[i].time += 31 * 5 + 30 * 5 + 29 + arr[i].DATE.date;
 				}
 			}
 		}	
 	}
 }
 
-void New_time(int full_time, int year, int day, int month)
+int New_time(int full_time, int year, int day, int month) //функция подсчета полного времени в днях;
 {
 	if (year % 4 != 0)
 	{
 		if (month == 1)
-			full_time += 31 + day;
+			full_time += day;
 		if (month == 2)
-			full_time += 28 + 31 + day;
+			full_time += 31 + day;
 		if (month == 3)
-			full_time += 31 * 2 + 28 + day;
+			full_time += 31 + 28 + day;
 		if (month == 4)
-			full_time += 31 * 2 + 28 + 30 + day;
+			full_time += 31 * 2 + 28 + day;
 		if (month == 5)
-			full_time += 31 * 3 + 30 + 28 + day;
+			full_time += 31 * 2 + 30 + 28 + day;
 		if (month == 6)
-			full_time += 31 * 3 + 30 * 2 + 28 + day;
+			full_time += 31 * 3 + 30 + 28 + day;
 		if (month == 7)
-			full_time += 31 * 4 + 30 * 2 + 28 + day;
+			full_time += 31 * 3 + 30 * 2 + 28 + day;
 		if (month == 8)
-			full_time += 31 * 5 + 30 * 2 + 28 + day;
+			full_time += 31 * 4 + 30 * 2 + 28 + day;
 		if (month == 9)
-			full_time += 31 * 5 + 30 * 3 + 28 + day;
+			full_time += 31 * 4 + 30 * 3 + 28 + day;
 		if (month == 10)
-			full_time += 31 * 6 + 30 * 3 + 28 + day;
+			full_time += 31 * 4 + 30 * 4 + 28 + day;
 		if (month == 11)
-			full_time += 31 * 6 + 30 * 4 + 28 + day;
+			full_time += 31 * 5 + 30 * 4 + 28 + day;
 		if (month == 12)
-			full_time += 31 * 7 + 30 * 4 + 28 + day;
+			full_time += 31 * 5 + 30 * 5 + 28 + day;
 	}
 
 	if (year % 4 == 0)
 	{
 		if (month == 1)
-			full_time += 31 + day;
+			full_time += day;
 		if (month == 2)
-			full_time += 29 + 31 + day;
+			full_time += 31 + day;
 		if (month == 3)
-			full_time += 31 * 2 + 29 + day;
+			full_time += 31 + 29 + day;
 		if (month == 4)
-			full_time += 31 * 2 + 29 + 30 + day;
+			full_time += 31 * 2 + 29 + day;
 		if (month == 5)
-			full_time += 31 * 3 + 30 + 29 + day;
+			full_time += 31 * 2 + 30 + 29 + day;
 		if (month == 6)
-			full_time += 31 * 3 + 30 * 2 + 29 + day;
+			full_time += 31 * 3 + 30 + 29 + day;
 		if (month == 7)
-			full_time += 31 * 4 + 30 * 2 + 29 + day;
+			full_time += 31 * 3 + 30 * 2 + 29 + day;
 		if (month == 8)
-			full_time += 31 * 5 + 30 * 2 + 29 + day;
+			full_time += 31 * 4 + 30 * 2 + 29 + day;
 		if (month == 9)
-			full_time += 31 * 5 + 30 * 3 + 29 + day;
+			full_time += 31 * 4 + 30 * 3 + 29 + day;
 		if (month == 10)
-			full_time += 31 * 6 + 30 * 3 + 29 + day;
+			full_time += 31 * 4 + 30 * 4 + 29 + day;
 		if (month == 11)
-			full_time += 31 * 6 + 30 * 4 + 29 + day;
+			full_time += 31 * 5 + 30 * 4 + 29 + day;
 		if (month == 12)
-			full_time += 31 * 7 + 30 * 4 + 29 + day;
+			full_time += 31 * 5 + 30 * 5 + 29 + day;
 	}
+
+	return full_time;
 }
 
 void easy()
@@ -454,9 +462,9 @@ void easy()
 		cin >> month;
 		cout << endl;
 
-	} while (isCorrect1(day, month, year) == 0); //подсчет в днях нынешнего года;
+	} while (isCorrect1(day, month, year) == 0); //проверка нынешней даты;
 
-	for (int i = 0; i <= year; i++) //находим нынешнее время в днях;
+	for (int i = 0; i < year; i++) //находим нынешнее время в днях;
 	{
 		if (i % 4 != 0)
 		{
@@ -469,7 +477,7 @@ void easy()
 		}
 	}
 
-	New_time(full_time, year, day, month); //функция нахождения нынешнего времени в днях;
+	long long int full_time_1 = New_time(full_time, year, day, month);
 
 	Old_time(arr, size); //функция нахождения времени жизни пенсионеров в днях;
 
@@ -492,15 +500,16 @@ void easy()
 
 	int count2 = 0;
 
-	write << "Информация о пенсионерах с пенсионным стажем больше 5 лет " << endl;
-	new_write << "Информация о пенсионерах с пенсионным стажем больше 5 лет " << endl;
+	write << "Информация о пенсионерах с пенсионным стажем больше 5 лет " << endl << endl;
+	new_write << "Информация о пенсионерах с пенсионным стажем больше 5 лет " << endl << endl;
 
 	for (int i = 0; i < size; i++) //если разница в днях между нынешним временем и временем жизни больше 65 лет, то так как пенсионный возраст начинается с 60 человек на пенсии больше 5 лет;
 	{
-		long long int different = full_time - arr[i].time;
+		
+		long long int different = full_time_1 - arr[i].time;
 
 		cout << "Пенсионер " << i + 1 << endl;
-		cout << "Полное время в днях - " << full_time << endl;
+		cout << "Полное время в днях - " << full_time_1 << endl;
 		cout << "Время жизни в днях - " << arr[i].time << endl;
 		cout << "65 лет в днях - " << sixty_year_time << endl;
 		cout << "Разница - " << different << endl << endl;
@@ -539,13 +548,15 @@ void easy()
 
 	if (count2 == 0)
 	{
-		write << "Нет пенсионеров с пенсионным стажем больше 5 лет";
-		new_write << "Нет пенсионеров с пенсионным стажем больше 5 лет";
+		write << endl << "Нет пенсионеров с пенсионным стажем больше 5 лет";
+		new_write << endl << "Нет пенсионеров с пенсионным стажем больше 5 лет";
 	}
 
 	cout << endl << "Соответсвенная информация вывелась в текстовый файл";
     cout << endl << endl;
 }
+
+//////////////////////////////////////////////////////////////////////////////
 
 void medium()
 {
@@ -614,6 +625,266 @@ void medium()
 	cout << endl << endl;
 }
 
+/////////////////////////////////////////////////////////////////////////////
+
+void hard()
+{
+	cout << "Необходимо создать 2 файла. В один из них необходимо записать N-ое кол-во матриц K-ого порядка\n";
+	cout << "В другое L-ое кол-во матриц. В файл с меньшим кол-вом матриц записать недостающее кол-во единичных матриц\n";
+	cout << "Вывести данные из файлов в консоль" << endl;
+
+	string read = "One_hard.txt"; //файлы с которыми мы будем работать;
+	string read_1 = "Two_hard.txt";
+
+	ofstream write, write_1;
+	
+	int quantity, quantity_2, order,** arr;
+	const int order_2 = 4;
+	int arr_1[order_2][order_2];
+	char symbol;
+
+	//Вводи кол-во матриц и размерность;
+	cout << endl << "Введите количество матриц для записи в 1-й файл: ";
+
+	cin >> quantity;
+
+	cout << endl << "Введите порядок данных матриц: ";
+
+	cin >> order;
+
+	cout << endl << "Введите кол-во матриц для записи во 2-й файл(их порядок равен 4): ";
+
+	cin >> quantity_2;
+
+	cout << endl;
+
+	//создаем двумерный динамический массив;
+	arr = new int* [order];
+
+	for (int i = 0; i < order; i++)
+	{
+		arr[i] = new int[order];
+	}
+
+	write.open(read);
+	write_1.open(read_1);
+
+	//записываем в оба файла матрицы;
+	if (!write.is_open()) //если не удается открыть файл - выдаем сообщение;
+	{
+		cout << endl << "Не удалось открыть или найти необходимый первый файл!" << endl;
+	}
+
+	else
+	{
+		cout << endl << "В файл № 1 записано заданное кол-во матриц" << endl << endl;
+		write << "Массивы первого файла: " << endl << endl;
+
+		for (int k = 0; k < quantity; k++)
+		{
+			for (int i = 0; i < order; i++)
+			{
+				for (int j = 0; j < order; j++)
+				{
+					arr[i][j] = rand() % 10 - 5;
+					write << arr[i][j] << "\t";
+				}
+
+				write << endl;
+			}
+
+			write << endl;
+		}
+    }
+
+	if (!write_1.is_open()) //если не удается открыть файл - выдаем сообщение;
+	{
+		cout << endl << "Не удалось открыть или найти необходимый второй файл!" << endl;
+	}
+
+	else
+	{
+		cout << endl << "В файл № 2 записано заданное кол-во матриц" << endl << endl;
+		write_1 << "Массивы второго файла: " << endl << endl;
+
+		for (int k = 0; k < quantity_2; k++)
+		{
+			for (int i = 0; i < order_2; i++)
+			{
+				for (int j = 0; j < order_2; j++)
+				{
+					arr_1[i][j] = rand() % 20 - 10;
+					write_1 << arr_1[i][j] << "\t";
+				}
+
+				write_1 << endl;
+			}
+
+			write_1 << endl;
+		}
+	}
+
+	//закрываем файлы;
+	write.close();
+	write_1.close();
+
+	//если кол-во матриц в первом файле не равно 1, то записываем в файл с большим кол-вом матриц недостающие единичные матрицы;
+	if (quantity != 1)
+	{
+		if (quantity > quantity_2)
+		{
+			int different = quantity - quantity_2;
+
+			write.open(read);
+
+			if (!write.is_open()) //если не удается открыть файл - выдаем сообщение;
+			{
+				cout << endl << "Не удалось открыть или найти первый файл для перезаписи!" << endl;
+			}
+
+			else
+			{
+				cout << endl << "В файл № 1 записано недостающее число матриц" << endl << endl;
+				write << "Недостающие массивы файла: " << endl << endl;
+
+				for (int k = 0; k < different; k++)
+				{
+					for (int i = 0; i < order; i++)
+					{
+						for (int j = 0; j < order; j++)
+						{
+							arr[i][j] = 1;
+							write << arr[i][j] << "\t";
+						}
+
+						write << endl;
+					}
+
+					write << endl;
+				}
+
+				write << endl << "Изначальные массивы данного файла: " << endl << endl;
+
+				for (int k = 0; k < quantity; k++)
+				{
+					for (int i = 0; i < order; i++)
+					{
+						for (int j = 0; j < order; j++)
+						{
+							arr[i][j] = rand() % 10 - 5;
+							write << arr[i][j] << "\t";
+						}
+
+						write << endl;
+					}
+
+					write << endl;
+				}
+			}
+
+			write.close();
+		}
+
+		if (quantity < quantity_2)
+		{
+			int different = quantity_2 - quantity;
+
+			write_1.open(read_1);
+
+			if (!write_1.is_open()) //если не удается открыть файл - выдаем сообщение;
+			{
+				cout << endl << "Не удалось открыть или найти второй файл для перезаписи!" << endl;
+			}
+
+			else
+			{
+				cout << endl << "В файл № 2 записано недостающее число матриц" << endl << endl;
+				write_1 << "Недостающие массивы файла: " << endl << endl;
+
+				for (int k = 0; k < different; k++)
+				{
+					for (int i = 0; i < order_2; i++)
+					{
+						for (int j = 0; j < order_2; j++)
+						{
+							arr_1[i][j] = 1;
+							write_1 << arr_1[i][j] << "\t";
+						}
+
+						write_1 << endl;
+					}
+
+					write_1 << endl;
+				}
+
+				write_1 << endl << "Изначальные массивы данного файла: " << endl << endl;
+
+				for (int k = 0; k < quantity_2; k++)
+				{
+					for (int i = 0; i < order_2; i++)
+					{
+						for (int j = 0; j < order_2; j++)
+						{
+							arr_1[i][j] = rand() % 10 - 5;
+							write_1 << arr_1[i][j] << "\t";
+						}
+
+						write_1 << endl;
+					}
+
+					write_1 << endl;
+				}
+			}
+
+			write_1.close();
+		}
+	}
+
+	//выводим данные в консоль;
+	ifstream console, console_1;
+
+	console.open(read);
+	console_1.open(read_1);
+
+	if (!console.is_open()) //если не удается открыть файл - выдаем сообщение;
+	{
+		cout << endl << "Не удалось открыть или найти первый файл!";
+	}
+
+	else
+	{
+		cout << endl << "Файл номер 1 открыт! В данном файле записано: " << endl << endl;
+
+		while (console.get(symbol)) //посимвольно считываем в консоль данные из файла;
+		{
+			cout << symbol;
+		}
+	}
+
+	if (!console_1.is_open()) //если не удается открыть файл - выдаем сообщение;
+	{
+		cout << endl << "Не удалось открыть или найти первый файл!";
+	}
+
+	else
+	{
+		cout << endl << "Файл номер 2 открыт! В данном файле записано: " << endl << endl;
+
+		while (console_1.get(symbol)) //посимвольно считываем в консоль данные из файла;
+		{
+			cout << symbol;
+		}
+	}
+
+	//закрываем файлы;
+	console.close();
+	console_1.close();
+
+	cout << endl << endl;
+}
+
+////////////////////////////////////////////////////////////////////////////
+
 int main()
 {
 	setlocale(LC_ALL, "RU");
@@ -623,7 +894,7 @@ int main()
 
 	do
 	{
-		cout << "Введите номер задания (easy - 1), (medium - 2): ";
+		cout << "Введите номер задания (easy - 1), (medium - 2), (hard - 3): ";
 
 		cin >> a;
 
@@ -638,6 +909,12 @@ int main()
 		if (a == 2)
 		{
 			medium();
+			count++;
+		}
+
+		if (a == 3)
+		{
+			hard();
 			count++;
 		}
 
@@ -671,3 +948,5 @@ int main()
 
 	} while (count == 0);
 }
+
+/////////////////////////////////////////////////////////////////
